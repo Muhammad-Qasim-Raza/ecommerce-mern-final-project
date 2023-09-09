9657d210461840d174ef22f72d9f73cbc66a604c
Muhammad Qasim Raza <115484433+Muhammad-Qasim-Raza@users.noreply.github.com>
2023-09-09 17:41:02 +0500

    final improvements
    final improvements

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //rest api
-app.use("*",function(req,res){
-  res.sendFile(path.join(__dirname,"./client/build/index.html"));
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 //PORT
